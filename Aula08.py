560a922c5feda547afc371d0059ee3fe6326546e
PedroVeicir <pedrohenriqueveicir@gmail.com>
2026-06-10 16:59:45 -0300

    Update Aula08.py

--- Aula08.py
+++ Aula08.py
@@ -1,2 +1,27 @@
-print("hi")
-print("hi")
+class carteira:
+  def __init__(self, moeda: str, saldo: float):
+    self.moeda = moeda
+    self.saldo = saldo
+
+  def converter(saldo, val_yuan):
+    if self.moeda == 'USD':
+      return self.saldo * 0.14
+    elif self.moeda == 'BRL':
+      return self.saldo * 0.85
+    else:
+      return val_yuan
+
+  def __add__(self, val_yuan):
+    if self.moeda != 'CNY':
+      return self.saldo + self.converter(val_yuan)
+    else:
+      return self.saldo + val_yuan
+
+  def __sub__(self, val_yuan):
+    if self.moeda != 'CNY':
+      return self.saldo - self.converter(val_yuan)
+    else:
+      return self.saldo - val_yuan
+
+carteira_usd = carteira("USD", 10.0)
+print(carteira_usd + 50.0)
